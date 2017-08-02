5eb4e9d4daced4a48f581bcb651e6f114a9cdd86
Ben Timby <btimby@gmail.com>
2017-08-02 14:07:37 -0400

    More robust disconnect() close() if shutdown() fails.

--- redis/connection.py
+++ redis/connection.py
@@ -576,6 +576,9 @@ class Connection(object):
             return
         try:
             self._sock.shutdown(socket.SHUT_RDWR)
+        except (OSError, socket.error):
+            pass
+        try:
             self._sock.close()
         except socket.error:
             pass
